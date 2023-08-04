04cc4e28e15f5f7f5bffe0e82fd7be3b3a3824ab
Paul Padamadan <paul@zephony.com>
2023-08-04 14:57:08 +0530

    Add functionality to double <Esc> when telescope is open

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,20 @@
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 -- vim.keymap.set('n', '<leader>ps', function()
 	-- builtin.grep_string({ search = vim.fn.input("Grep > ") });
 -- end)
 vim.keymap.set('n', '<leader>ps', builtin.live_grep)
+
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+            },
+        },
+    },
+})
 
