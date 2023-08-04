75d37a174079730bdf5988cf4e2d488f135c7279
Paul Padamadan <paul@zephony.com>
2023-08-04 21:42:53 +0530

    Change telescope git_files toggle

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,7 +4,7 @@ local actions = require("telescope.actions")
 vim.keymap.set('n', '<leader>ps', builtin.find_files, {})
 --Disabling as for some reason Ctrl + p is causing delay in opening window
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<C-;>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>lg', builtin.live_grep)
 -- vim.keymap.set('n', '<leader>ps', function()
 -- builtin.grep_string({ search = vim.fn.input("Grep > ") });
