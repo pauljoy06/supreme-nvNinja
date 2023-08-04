10b8e2ae31a89a993f89c7f42417aa46b8ac2ff4
Paul Padamadan <paul@zephony.com>
2023-08-04 20:25:39 +0530

    Remap telescope trigger shortcut

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,20 +1,25 @@
 local builtin = require('telescope.builtin')
 local actions = require("telescope.actions")
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.find_files, {})
+--Disabling as for some reason Ctrl + p is causing delay in opening window
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>lg', builtin.live_grep)
 -- vim.keymap.set('n', '<leader>ps', function()
-	-- builtin.grep_string({ search = vim.fn.input("Grep > ") });
+-- builtin.grep_string({ search = vim.fn.input("Grep > ") });
 -- end)
-vim.keymap.set('n', '<leader>ps', builtin.live_grep)
 
 require("telescope").setup({
     defaults = {
         mappings = {
             i = {
-                ["<esc>"] = actions.close,
+                ['<esc>'] = actions.close,
+                ['<leader>tt'] = require('telescope.actions.layout').toggle_preview
             },
         },
+        preview = {
+            hide_on_startup = false     -- hide previewer when picker starts
+        }
     },
 })
-
