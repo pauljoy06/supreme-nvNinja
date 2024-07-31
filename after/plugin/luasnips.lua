bcb795b265ef45de106db27fe63562ef6a617c4d
Paul Padamadan <paul.padamadan@programming.com>
2024-07-31 19:35:48 +0530

    Add snippet navigation for luasnip

--- after/plugin/luasnips.lua
+++ after/plugin/luasnips.lua
@@ -12,3 +12,14 @@ require('luasnip').filetype_extend("typescript", { "html" }) -- Not working
 
 require('luasnip.loaders.from_lua').load({ paths = './my_snips.lua' })
 
+local ls = require('luasnip')
+
+vim.keymap.set({"i"}, "<C-k>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-l>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump(-1) end, {silent = true})
+
+-- vim.keymap.set({"i", "s"}, "<C-E>", function()
+-- 	if ls.choice_active() then
+-- 		ls.change_choice(1)
+-- 	end
+-- end, {silent = true})
--- after/plugin/my_snips.lua
+++ after/plugin/my_snips.lua
@@ -2,6 +2,7 @@ local ls = require('luasnip')
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local c = ls.choice_node
 
 -- Define a simple snippet
 ls.add_snippets(
