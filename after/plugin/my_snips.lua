43e7a5e05913f1d97b1fde50a6eed68ef3d4199c
Paul Padamadan <paul.padamadan@programming.com>
2024-08-29 20:29:02 +0530

    Add new snippet for react component

--- after/plugin/my_snips.lua
+++ after/plugin/my_snips.lua
@@ -3,6 +3,7 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 -- Define a simple snippet
 ls.add_snippets(
@@ -12,3 +13,25 @@ ls.add_snippets(
     })
   }
 )
+
+
+
+ls.add_snippets("all", {
+    s("reactcomp", fmt([[
+        import React from 'react';
+
+        const {} = () => {{
+            return <div>
+                {}
+            </div>
+        }}
+
+        export default {};
+    ]], {
+        i(1, "ComponentName"),
+        i(3, "Component Content"),
+        i(2, "ComponentName")  -- This reuses the first insert node
+    })),
+})
+
+
