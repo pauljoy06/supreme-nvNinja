75fe95d682486b7a0db9a94f43e55defd3f5754a
Paul Padamadan <paul.padamadan@programming.com>
2024-09-03 16:01:08 +0530

    Add shortcut to trigger DBUI

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -185,3 +185,5 @@ vim.keymap.set("n", "<M-.>", "<c-w>5>")
 vim.keymap.set("n", "<M-t>", "<C-W>+")
 vim.keymap.set("n", "<M-s>", "<C-W>-")
 vim.keymap.set("n", "<M-s>", "<C-W>-")
+
+vim.keymap.set("n", "<leader>db", ":DBUI<CR>")
