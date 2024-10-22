9632cfba694bc8b17e1fbacfccb5a2294742866c
Paul Padamadan <paul.padamadan@programming.com>
2024-10-22 16:02:33 +0530

    Auto switch between git_files and find_files in telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,21 @@
 local builtin = require('telescope.builtin')
 local actions = require("telescope.actions")
 
+
+-- Function to dynamically switch between 'git_files' and 'find_files'
+local function project_files()
+    local opts = {} -- You can pass options here if needed
+    local is_git_repo = vim.fn.system('git rev-parse --is-inside-work-tree')
+
+    if vim.v.shell_error == 0 then
+        -- If inside a git repo, search for Git-tracked files
+        builtin.git_files(opts)
+    else
+        -- If not a git repo, search for regular files
+        builtin.find_files(opts)
+    end
+end
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc='Search all files in folder'})
 --Disabling as for some reason Ctrl + p is causing delay in opening window
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
@@ -8,6 +23,9 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc='Search all files in
 -- vim.keymap.set('n', '<C-;>', builtin.git_files, {})
 --  Windows specific configuration below
 vim.keymap.set('n', '<A-;>', builtin.git_files, { desc='Search for files in git' })
+-- Map the function to a keybinding (e.g., Alt-;)
+vim.keymap.set('n', '<A-;>', project_files, { desc = 'Search project files intelligently' })
+
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc='Live grep search for project' } )
 vim.keymap.set('n', '<leader>qf', builtin.quickfix, { desc='Open quickfix list' })
 -- vim.keymap.set('n', '<leader>ps', function()
