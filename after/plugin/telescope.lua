dce0c2ecb10c9b153399a2499ba34fba79e57fa6
Paul Padamadan <paul.padamadan@programming.com>
2024-10-28 16:47:11 +0530

    Add quickfix related options

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -18,25 +18,24 @@ end
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc='Search all files in folder'})
 --Disabling as for some reason Ctrl + p is causing delay in opening window
+
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 -- vim.keymap.set('n', '<C-;>', builtin.git_files, {})
 -- vim.keymap.set('n', '<C-;>', builtin.git_files, {})
+
 --  Windows specific configuration below
 vim.keymap.set('n', '<A-;>', builtin.git_files, { desc='Search for files in git' })
+
 -- Map the function to a keybinding (e.g., Alt-;)
 vim.keymap.set('n', '<A-;>', project_files, { desc = 'Search project files intelligently' })
 
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc='Live grep search for project' } )
-vim.keymap.set('n', '<leader>qf', builtin.quickfix, { desc='Open quickfix list' })
--- vim.keymap.set('n', '<leader>ps', function()
--- builtin.grep_string({ search = vim.fn.input("Grep > ") });
--- end)
--- vim.api.nvim_set_keymap('n',
---     '<C-r>',
---     [[<cmd>lua require('telescope.actions').reload()<CR>]],
---     { noremap = true, silent = true }
--- )
 vim.keymap.set('n', '<leader>km', ':Telescope keymaps<CR>', {desc='Open Telescope keymaps'})
+
+vim.keymap.set('n', '<leader>qf', builtin.quickfix, { desc='Open quickfix list' })
+vim.keymap.set('n', ']q', ':cnext<CR>', { noremap = true, silent = true, desc = 'Next quickfix entry' })
+vim.keymap.set('n', '[q', ':cprev<CR>', { noremap = true, silent = true, desc = 'Previous quickfix entry' })
+
 
 require("telescope").setup({
     defaults = {
@@ -48,30 +47,14 @@ require("telescope").setup({
                 ['<leader>ttp'] = require('telescope.actions.layout').toggle_preview
             },
         },
-        -- vimgrep_arguments = {
-        --     'rg',
-            -- '--hidden',           -- Search hidden files
-            -- '--no-heading',
-            -- '--with-filename',
-            -- '--line-number',
-            -- '--column',
-            -- '--smart-case',
-            -- '--ignore-file', '.gitignore', -- Use .gitignore to skip files
-            -- '--ignore-dir', 'build',       -- Additionally ignore 'build' directory
-            -- '--ignore-dir', 'logs'         -- Additionally ignore 'logs' directory
-        -- },
-        -- vimgrep_arguments = vim.tbl_extend('force', require('telescope.config').values.vimgrep_arguments, {
-        --     '--ignore-dir', 'node_modules',
-        --     '--ignore-dir', 'dist',
-        -- })
-
+        previewer = false,
     },
     color_devicons = true,
     -- pickers = {
     --     find_files = { }
     -- },
     preview = {
-        hide_on_startup = false     -- hide previewer when picker starts
+        hide_on_startup = true     -- hide previewer when picker starts
     }
 })
 
