9267d1183fd3b17f1c92ebe52afaf119e7f9a452
Paul Padamadan <paul@zephony.com>
2025-11-21 07:30:55 +0530

    Add custom LuaSnip snippets and keymaps

---
+++ after/plugin/my_snips.lua
@@ -0,0 +1,60 @@
+local ls = require('luasnip')
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local c = ls.choice_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+-- Define a simple snippet
+ls.add_snippets(
+  'all',  {
+    s('impr', {
+      t("import React from 'react';"),
+    })
+  }
+)
+
+ls.add_snippets("all", {
+    s("rc", fmt([[
+        const {} = (props) => {{
+            return <div className='{}'>
+                {}
+            </div>
+        }}
+    ]], {
+        i(1, "ComponentName"),
+        i(2, "className"),
+        i(3, "content")
+    })),
+})
+
+ls.add_snippets("all", {
+    s("rcn", fmt([[
+        import React from 'react';
+
+        const {} = (props) => {{
+            return <div className='{}'>
+                {}
+            </div>
+        }}
+
+        export default {};
+    ]], {
+        i(1, "ComponentName"),
+        i(3, "className"),
+        i(4, "content"),
+        i(2, "ComponentName")
+    })),
+})
+
+ls.add_snippets("javascript", {
+  s("us", {
+    t({"const ["}),
+    i(1, "state"),     -- initial state variable name
+    t({", set"}),
+    i(2, "State"),     -- state setter function
+    t({"] = useState("}),
+    i(3, "initialValue"), -- initial value for the state
+    t({");"})
+  })
+})
