301499e6bc04e695c204139df88dd742bf81cbdb
Paul Padamadan <paul@zephony.com>
2025-11-21 07:30:34 +0530

    Expand Treesitter parsers and enable indent

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,17 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-  ensure_installed = { "vimdoc", "javascript", "typescript", "python", "lua"},
+  ensure_installed = {
+        "vimdoc",
+        "javascript",
+        "typescript",
+        -- "typescriptreact",
+        "tsx",
+        "python",
+        "lua",
+        "html",
+        "css"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -18,5 +28,8 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,                -- Enable Treesitter-based indentation
   },
 }
