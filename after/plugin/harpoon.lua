e2828dbe22d0fecb2fc151ed4e17257b1885b6b5
Paul Padamadan <paul@zephony.com>
2025-11-22 21:29:23 +0530

    Add leader-based fallback mappings for Harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,3 +8,9 @@ vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-m>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
+
+-- Fallback mappings for terminals that swallow <C-h>/<C-t>/<C-m>/<C-n>
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon file 1" })
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon file 2" })
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon file 3" })
+vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Harpoon file 4" })
