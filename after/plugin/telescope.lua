9e555d3760f3d5cf1144cdd278646c64b21cd196
Paul Padamadan <paul@zephony.com>
2025-11-22 12:31:15 +0530

    Restore Windows-specific Alt-; mapping for project files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -21,6 +21,12 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc='Search all files in
 -- vim.keymap.set('n', '<C-;>', builtin.git_files, {})
 -- vim.keymap.set('n', '<C-;>', builtin.git_files, {})
 
+--  Windows specific configuration below
+vim.keymap.set('n', '<A-;>', builtin.git_files, { desc='Search for files in git' })
+
+-- Map the function to a keybinding (e.g., Alt-;)
+vim.keymap.set('n', '<A-;>', project_files, { desc = 'Search project files intelligently' })
+
 -- Preferred project files shortcut
 vim.keymap.set('n', '<leader>pp', project_files, { desc = 'Project files' })
 
